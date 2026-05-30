ae9fcf2827e41aa6b31b95f41be156a6a9df47f4
saikumarvarahabatla <40891012+saikumarvarahabatla@users.noreply.github.com>
2026-05-30 18:09:13 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+months = [
+    "January", "February", "March", "April",
+    "May", "June", "July", "August",
+    "September", "October", "November", "December"
+]
+
+for month in months:
+    print(month)
